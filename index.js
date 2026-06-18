073fd3bac33b6866feacab592442a01e89f03cdf
Raju-ahamed <rajuahamed7320@gmail.com>
2026-06-18 14:07:02 +0600

    2nd file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const phones = require('./phone.json');
 const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("hello hi world")
+});
+app.get('/phone', (req, res) => {
+    res.send(phones);
 })
 app.listen(port, () => {
     console.log(`example app lisiting on port ${port}`)
